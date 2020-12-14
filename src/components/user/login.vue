e7932a4511215972b80085bbf8ec758a3a2442aa
mg <820076943@qq.com>
2020-12-14 17:46:25 +0800

    脚手架搭建+登录界面

---
+++ src/components/user/login.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="login">
+    <div class="login-form">
+      <div class="my-login">
+        <img src="http://www.minggang.top:8000/img/common/logo.png" alt="" />
+      </div>
+      <div class="form-box">
+        <el-form
+          label-position="top"
+          label-width="80px"
+          :model="formLabelAlign"
+          ref="myForm"
+        >
+          <el-form-item label="用户名:">
+            <el-input v-model="formLabelAlign.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码:">
+            <el-input
+              type="password"
+              v-model="formLabelAlign.password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item class="submit">
+            <el-button type="primary" class="submitBtn" @click="submitForm('myForm')"
+              >登录</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formLabelAlign: {
+        username: "root",
+        password: "123456",
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+     console.log(this.formLabelAlign)
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  width: 100%;
+  height: 100vh;
+  background: url(../../assets/loginBg.jpg);
+  background-size: 100% 100%;
+  position: relative;
+  .login-form {
+    position: absolute;
+    width: 400px;
+    height: 400px;
+    background-color: #fff;
+    top: 50%;
+    margin-top: -200px;
+    right: 50%;
+    margin-right: -200px;
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    .my-login {
+      width: 150px;
+      height: 116px;
+    }
+    .form-box {
+      width: 90%;
+      height: 100%;
+      .submitBtn {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
